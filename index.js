84c04da49feda65f307b56a9771f44431bbe61fe
juanjomcw <37007743+juanjomcw@users.noreply.github.com>
2019-09-07 00:20:12 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+const tmi = require("tmi.js");
+const options = require("./option"); //Your options file
+
+//Connect to twitch server
+const client = new tmi.client(options);
+client.connect();
+
+client.on('chat', function(channel, user, message, self){
+    if(self) return;
+    if(message === "!test"){
+        client.say("Juanjomcw", "Hey esto es una prueba moondaHYPE")
+    }
+});
+
+client.on('chat', function(channel, user, message, self){
+    if(self) return;
+    if(message === "hola"){
+        client.say("Juanjomcw", "hola @" + user.username + " moondaHYPE")
+    }
+});
+
+client.on('chat', function(channel, user, message, self){
+    if(self) return;
+    setInterval( () => {
+        client.say("Juanjomcw", "!discord")
+    }, 600000);
+});
+
+client.login(process.env.TOKEN);
