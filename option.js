2fb6ba7b7232e417bcb909c528f0ec794bf0ee71
juanjomcw <37007743+juanjomcw@users.noreply.github.com>
2019-09-07 00:29:43 -0600

    Create option.js

---
+++ option.js
@@ -0,0 +1,16 @@
+const options = {
+    options: {
+      debug: true
+    },
+    connection: {
+      reconnect: true
+    },
+    identity: {
+      username: "NAME",
+      password: "TOKEN"
+    },
+    channels: ['Juanjomcw']
+  };
+  
+  
+  module.exports = options;
